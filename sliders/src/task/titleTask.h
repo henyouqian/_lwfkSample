c26783d1156cb08f761e24d01ab419b900018b2f
liwei <henyouqian@gmail.com>
2013-10-16 00:07:22 +0800

    lw

---
+++ sliders/src/task/titleTask.h
@@ -0,0 +1,35 @@
+#ifndef __LW_TITLE_TASK_H__
+#define __LW_TITLE_TASK_H__
+
+#include "lwfk/lwTask.h"
+
+namespace lw {
+    class Sprite;
+    class Sound;
+}
+
+class TitleTask: public lw::Task
+{
+public:
+    TitleTask();
+    ~TitleTask();
+    
+private:
+    virtual void vStart();
+    virtual void vStop();
+    virtual void vUpdate();
+    virtual void vDraw();
+    
+    virtual void vTouchBegan(const lw::Touch &touch);
+    virtual void vTouchMoved(const lw::Touch &touch);
+    virtual void vTouchEnded(const lw::Touch &touch);
+    virtual void vTouchCanceled(const lw::Touch &touch);
+    
+    lw::Sprite *_pSprite;
+    lw::Sound *_pSound;
+};
+
+extern TitleTask gTitleTask;
+
+
+#endif //__LW_TITLE_TASK_H__
